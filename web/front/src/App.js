00d6a0c1a11c7513771e6f4fba1498791671a4c5
Corey Livinghouse <clivinghouse@mertandhouse.com>
2024-06-23 14:28:20 -0400

    testing commits

--- web/front/src/App.js
+++ web/front/src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+        </Container>
+      </Navbar>
   );
 }
 
